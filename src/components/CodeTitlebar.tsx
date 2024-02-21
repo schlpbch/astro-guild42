14f28877392b0d3c8b64815f34ef85e62823419c
Andreas Schlapbach <schlpbch@gmail.com>
2024-02-21 21:42:39 +0100

    Update CodeTitlebar component buttons

--- src/components/CodeTitlebar.tsx
+++ src/components/CodeTitlebar.tsx
@@ -4,10 +4,17 @@ export default function CodeTitlebar() {
   return (
     <div className="flex-none border-b border-slate-500">
       <div className="flex h-8 items-center space-x-1.5">
-        <button className="h-2.5 w-2.5 rounded-full bg-red-500 hover:bg-red-400"></button>
-        <button className="h-2.5 w-2.5 rounded-full bg-yellow-500 hover:bg-yellow-400"></button>
+        <button
+          className="h-2.5 w-2.5 rounded-full bg-red-500 hover:bg-red-400"
+          aria-label="Close"
+        ></button>
+        <button
+          className="h-2.5 w-2.5 rounded-full bg-yellow-500 hover:bg-yellow-400"
+          aria-label="Maximize"
+        ></button>
         <button
           className="h-2.5 w-2.5 rounded-full bg-green-500 hover:bg-green-400"
+          aria-label="Minimize"
           onClick={() => {
             alert(
               "42 is the 'Answer to the Ultimate Question of Life, the Universe, and Everything'"
