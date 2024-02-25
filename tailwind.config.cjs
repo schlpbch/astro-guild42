438f17f23627b685eed428ec3bf7fd8c119b0b5a
Andreas Schlapbach <schlpbch@gmail.com>
2024-02-25 16:32:50 +0100

    Update content patterns in tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,7 @@ function withOpacity(variableName) {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
   theme: {
     // Remove the following screen breakpoint or add other breakpoints
     // if one breakpoint is not enough for you
