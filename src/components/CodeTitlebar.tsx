139cd0b710eadddcfdbcbb0682c07860d25e43b2
Andreas Schlapbach <schlpbch@gmail.com>
2024-02-25 09:59:14 +0100

    Refactor Overlay42 component

--- src/components/CodeTitlebar.tsx
+++ src/components/CodeTitlebar.tsx
@@ -21,7 +21,7 @@ export default function CodeTitlebar() {
           onClick={() => setShowDialog(true)}
         ></button>
       </div>
-      {showDialog && <Overlay42 onClick={() => setShowDialog(false)} />}
+      {showDialog && <Overlay42 />}
     </div>
   );
 }
--- src/components/Overlay42.tsx
+++ src/components/Overlay42.tsx
@@ -2,11 +2,6 @@ import React, { useState } from "react";
 
 export default function Overlay42() {
   const [open, setOpen] = useState(true);
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   if (open) {
     return (
       <div
@@ -18,28 +13,24 @@ export default function Overlay42() {
         <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <div className="relative transform overflow-hidden rounded-lg bg-black px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <div className="relative transform overflow-hidden rounded-lg bg-black p-4 shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
               <div>
-                <div className="text-center">
-                  <h3
-                    className="my-5 inline-block bg-gradient-to-r from-blue-400 to-cyan-500 bg-clip-text text-5xl font-bold tracking-tighter text-transparent sm:my-8 sm:text-5xl"
-                    id="modal-title"
-                  >
-                    42
-                  </h3>
-                  <div className="mt-2">
-                    <p className="text-md text-gray-200">
-                      Answer to the Ultimate Question of Life, the Universe, and
-                      Everything
-                    </p>
-                  </div>
-                </div>
+                <h1
+                  className="my-5 inline-block bg-gradient-to-r from-blue-400 to-cyan-500 bg-clip-text text-5xl font-bold tracking-tighter text-transparent"
+                  id="modal-title"
+                >
+                  42
+                </h1>
+                <p className="text-lg text-gray-200">
+                  Answer to the Ultimate Question of Life, the Universe, and
+                  Everything
+                </p>
               </div>
-              <div className="mt-5 text-center sm:mt-6 ">
+              <div className="mt-5">
                 <button
                   type="submit"
-                  className="relative p-4 underline decoration-dashed underline-offset-4 hover:text-skin-accent focus-visible:p-1"
+                  className="relative p-4 text-gray-200 underline decoration-dashed underline-offset-4 hover:text-skin-accent focus-visible:p-1"
                   onClick={() => setOpen(false)}
                 >
                   Close
