11644f54ced9b011bec4c00fb715f9df31f5ce71
Andreas Schlapbach <schlpbch@gmail.com>
2024-02-25 16:35:59 +0100

    Remove unused font dependency and update tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,3 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
-
 function withOpacity(variableName) {
   return ({ opacityValue }) => {
     if (opacityValue !== undefined) {
@@ -20,9 +18,7 @@ module.exports = {
     },
 
     extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      },
+
       textColor: {
         skin: {
           base: withOpacity("--color-text-base"),
