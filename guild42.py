b1949565b29d760f1b42b41d34becff0643487b9
Andreas Schlapbach <schlpbch@gmail.com>
2024-04-14 16:23:40 +0200

    feat: add ultimateQuestion():

---
+++ guild42.py
@@ -0,0 +1,18 @@
+'''
+ We organize events in Berne related to software engineering. The focus is to exchange knowledge 
+ and experience among software developers and software companies. We believe this increases the 
+ attractiveness of Berne as a location to develop great software!
+'''
+
+def ultimateQuestion():
+  answer = 0
+  counter = 0
+  while counter < 9:
+    if (answer / 4) < 7:
+      answer += 4
+    else:
+      answer += (counter - 1)
+    counter += 1
+  return answer + 1
+
+print(ultimateQuestion())
