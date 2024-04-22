8021a15569045b2dfafd2045c9468c73aaccb389
Andreas Schlapbach <schlpbch@gmail.com>
2024-04-22 12:09:36 +0200

    feat: comment magical numbers

--- guild42.py
+++ guild42.py
@@ -1,13 +1,13 @@
 '''
- We organize events in Berne related to software engineering. The focus is to exchange knowledge 
- and experience among software developers and software companies. We believe this increases the 
- attractiveness of Berne as a location to develop great software!
+We organize events in Berne related to software engineering. The focus is to exchange knowledge 
+and experience among software developers and software companies. We believe this increases the 
+attractiveness of Berne as a location to develop great software!
 '''
 
 def ultimateQuestion():
   answer = 0
   counter = 0
-  while counter < 9:
+  while counter < 9:     # TODO: document magic numbers
     if (answer / 4) < 7:
       answer += 4
     else:
