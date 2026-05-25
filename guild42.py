3c8102794205f2bc85702dd3bef7b2e5d544754c
Andreas Schlapbach <schlpbch@gmail.com>
2026-05-25 08:39:47 +0200

    fix: correct docstring formatting and maintain code structure in ultimateAnswer function

--- guild42.py
+++ guild42.py
@@ -1,18 +1,17 @@
-'''
-We organize events in Berne related to software engineering. The focus is to exchange knowledge 
-and experience among software developers and software companies. We believe this increases the 
+"""
+We organize events in Berne related to software engineering. The focus is to exchange knowledge
+and experience among software developers and software companies. We believe this increases the
 attractiveness of Berne as a location to develop great software!
-'''
+"""
+
 
 def ultimateAnswer():
-  answer = 0
-  counter = 0
-  while counter < 9:     # TODO: document magic numbers
-    if (answer / 4) < 7:
-      answer += 4
-    else:
-      answer += (counter - 1)
-    counter += 1
-  return answer + 1
-
-print(ultimateAnswer())
+    answer = 0
+    counter = 0
+    while counter < 9:  # TODO: document magic numbers
+        if (answer / 4) < 7:
+            answer += 4
+        else:
+            answer += counter - 1
+        counter += 1
+    return answer + 1
